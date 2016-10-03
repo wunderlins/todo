c79cc1658bf411a0e0c6e2c846eb985f439f6108
wus <swunderlin@gmail.com>
2016-10-03 18:17:41 +0200

    added host/port

--- bin/uwsgi.py
+++ bin/uwsgi.py
@@ -25,8 +25,20 @@ def hello_world():
 	return 'Hello, World!'
 
 def main():
-	app.run()
+	port = 8020
+	host = "0.0.0.0"
+	
+	if len(sys.argv) > 1:
+		port = int(sys.argv[1])
+		print " * port: %d" % port
+	
+	if len(sys.argv) > 2:
+		host = sys.argv[2]
+		print " * host: %s" % host
+	
+	app.run(port=port, host=host)
 
 if __name__ == "__main__":
 	print " * Starting ..."
 	main()
+	
